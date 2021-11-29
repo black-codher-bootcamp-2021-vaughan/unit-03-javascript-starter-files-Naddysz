a48a664dd3b93626b286b8ab0953a14b62311226
Naddysz <44329061+Naddysz@users.noreply.github.com>
2021-11-29 20:19:28 +0000

    multiple parameters in  function

--- session-3/index.js
+++ session-3/index.js
@@ -1,6 +1,8 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
 
+//Task 1 
+
 let a = 5;
 const b = 10;
 let c = a + b;
@@ -11,6 +13,7 @@ a = 20;
 c = a + b;
 console.log(c)
 
+//Task 2
 function sayHey() {
     console.log("Hey!");
 }
@@ -23,3 +26,35 @@ function conversation() {
 
 conversation();
 
+//Task 3
+
+function conversation(name,topic) {
+    sayHey();
+    console.log("Do you like" + topic + "?");
+}
+
+conversation("Erika", "pizza");
+
+conversation("John", "cake");
+
+function addTwoNumbers(x,y) {
+    const answer = x + y;
+    return"The answer is: " + answer;
+}
+ console.log(addTwoNumbers (1,1)); 
+
+
+
+ function futureAge(name,age) {
+    const answer = age + 5;
+    return "Hi " + name+"! You will be " + answer + " years old in 5 years";
+ }
+
+ // Two  ways of excuting code
+ var Nads = futureAge("Nadra", 22);
+ console.log(Nads);
+
+ console.log(futureAge("Nadra", 22));
+
+
+ 
