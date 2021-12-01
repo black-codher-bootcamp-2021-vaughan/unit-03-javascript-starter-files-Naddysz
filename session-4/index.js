6d25cf0238c8450f18870ac3b618c42c7bb0d204
Naddysz <44329061+Naddysz@users.noreply.github.com>
2021-12-01 19:27:56 +0000

    objects and operators

--- session-4/index.js
+++ session-4/index.js
@@ -1,42 +1,59 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+const personA = {
+    name: "Yahya", 
+    age: "18",
+    location: "Birmingham",
+    likes: "manga",
+};
 
+const personB = { 
+    name: "Musab",
+    age: "13",
+    location: "Kenya",
+    likes: "anatomy",
+};
 
+const personC = {
+    name: "Nadia",
+    age: "9",
+    location: "Kenya",
+    likes: "skating",
+};
 
+const personD = {
+    name: "Munir",
+    age: "11",
+    location: "Kenya",
+    likes: "youtube",
+};
 
+function biography(person) {
+    return "Hi, my name is " + person.name + ". I am " + person.age + " years old, i live in " + person.location + " and I like " + person.likes;
+}
 
-
-
-
-
-
-
-
-
-
-
+console.log(biography(personA));
+console.log(biography(personB));
+console.log(biography(personC));
+console.log(biography(personD));
 
 
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+ const x = 6;
+ const y = 4;
 
-// const addition = x + y;
+ const addition = x + y;
+ console.log("Answer for addition of x + y is " + addition);
+ 
+ const subtraction = x - y;
+ console.log("Answer for subtraction of x - y is " + subtraction);
 
-// console.log("Addition: x + y " + addition);
+ const multiplication = x * y;
+ console.log("Answer for multiplication of x * y is " + multiplication);
 
-// const subtraction = x - y;
-
-// console.log("Subtraction: x - y " + subtraction);
-
-// const multiplication = x * y;
-
-// console.log("multiplication: x * y " + multiplication);
-
-// const division = x * y;
-
-// console.log("Division: x + y " + division);
+ const division = x * y;
+ console.log("Answer for division of x + y is " + division);
 
 
 
